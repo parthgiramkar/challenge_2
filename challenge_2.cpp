662694e839e47730ab5187138a45e30b15f13baa
parthgiramkar <imparthgiramkar@gmail.com>
2024-09-10 20:39:07 +0530

    Update challenge_2.cpp
    using xor operator

--- challenge_2.cpp
+++ challenge_2.cpp
@@ -1,3 +1,23 @@
-jygfrtdr5d
-shvcxvxj
-vhnhc hb
+#include<bits/stdc++.h>
+using namespace std;
+
+int missingnumber(const vector<int>&a, int N) {
+    int n=N-1;
+    int xor1=0;
+    int xor2=0;
+    for(int i=0;i<n;i++) {
+        xor1=xor1^a[i];
+    }
+    for(int i=1;i<=N;i++) {
+        xor2=xor2^i;
+    }
+    return xor1^xor2;
+    
+}
+int main() {
+    vector<int>a={1,2,4,5};
+    int N =5;
+    cout<<"Missing number-"<<missingnumber(a,N) ;
+    return 0;
+} 
+
